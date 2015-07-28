b0a94000c70e8dc1854172c546b64148bd74611f
Liam Dickson <liam.s.dickson@gmail.com>
2015-07-27 23:04:30 -0400

    Submit Button

--- src/ContactWell.js
+++ src/ContactWell.js
@@ -1,23 +1,34 @@
 'use strict';
 
+var _ = require('underscore');
 var React = require('react');
 var {Well, Input, ButtonInput} = require('react-bootstrap');
+var ReCATPCHA = require("react-google-recaptcha");
 var partial = require('lodash.partial');
 var WorkCell = require('./WorkCell');
 
 var WorkWell = module.exports = React.createClass({
+	validate: function (value) {
+		if (value) {
+			this.setState({value: true});
+		}
+	}
 	render: function () {
 		return (
 			<Well>
 				<h2 id='contact'>Contact Me</h2>
 				<form>
-					<Input type='text' label='Name' />
-					<Input type='email' label='Email' />
-					<Input className='contact-message' type='textarea' label='Message' />
-					<div className='contact-recaptcha'>
-						<div className='g-recaptcha' data-sitekey='6LduPvoSAAAAAOAlarIyHgQuhufOPoRdsju1STBC'></div>
-					</div>
-					<ButtonInput bsStyle='primary' type='submit' value='Submit' />
+					<Input onBlur={_.partial(this.validate,'name')} type='text' label='Name' />
+					<Input onBlur={_.partial(this.validate,'email')} type='email' label='Email' />
+					<Input onBlur={_.partial(this.validate,'message')} className='contact-message' type='textarea' label='Message' />
+					<ReCATPCHA
+						className='contact-recaptcha'
+						refs="recaptcha"
+						sitekey="6LduPvoSAAAAAOAlarIyHgQuhufOPoRdsju1STBC"
+						onChange={_.partial(this.validate,'captcha')}  />
+					<ButtonInput disabled={
+						!(this.state.name && this.state.email && this.state.message && this.state.captcha)
+					} bsStyle='primary' type='submit' value='Submit' />
 				</form>
 			</Well>
 		)
